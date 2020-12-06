937c151dfc02596061ad8eca0866f2c35d369a90
jprebelo <joaopaulo.rebelo@saphety.com>
2020-12-06 17:42:47 +0000

    upadating styles and optimizing script

--- script.js
+++ script.js
@@ -2,62 +2,66 @@
 const div = document.createElement('div')
 div.classList.add('numbers-wrapper')
 
-// insert div wrap to existing element in html
+// insert div wrap to already existing element in html
 const placer = document.querySelector('.content-area')
+placer.insertAdjacentElement('afterbegin', div)
 
-// create div to add balls numbers
+// create div to add balls (numbers)
 const divNumbers = `
     <div></div>
 `
 div.innerHTML = divNumbers
 
-// add div for balls numbers to html
+// add div for balls to html
 const divNumbersEl = div.querySelector('div')
 divNumbersEl.classList.add('balls')
 
-// create ul to place generated numbers
-const ul_Numbers = `
-    <ul>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-     <li></li>
-    </ul>
-`
-divNumbersEl.innerHTML = ul_Numbers;
+// create div to add stars
+const divStars = ` 
+<div></div>
+      `
+div.insertAdjacentHTML('beforeend', divStars)
 
-// add ul element to html
-const ul_NumberEl = divNumbersEl.querySelector('ul')
-
-const ball1 = ul_NumberEl.querySelector('ul li:nth-child(1)')
-const ball2 = ul_NumberEl.querySelector('ul li:nth-child(2)')
-const ball3 = ul_NumberEl.querySelector('ul li:nth-child(3)')
-const ball4 = ul_NumberEl.querySelector('ul li:nth-child(4)')
-const ball5 = ul_NumberEl.querySelector('ul li:nth-child(5)')
-
-ball1.classList.add('ball')
-ball2.classList.add('ball')
-ball3.classList.add('ball')
-ball4.classList.add('ball')
-ball5.classList.add('ball')
+// add div for stars to html
+const divStarsEl = div.querySelector('div:nth-child(2)')
+divStarsEl.classList.add('stars')
 
 
 // get button to start generate numbers
 const bt = document.getElementById('spark')
+
+// get button to refresh page
 const btReset = document.getElementById('reset')
+
 // create array to store numbers
 const numbers = []
-let ball
+// create array to store stars
+const stars = []
 
+// variable to store ball
+let ball
+// variable to store star
+let star
+
+// generate random number between 1 and 50
 function generateNumber() {
-    ball = Math.floor(Math.random() * 10);
+    ball = Math.floor(Math.random() * 50) + 1
+}
+// generate random star between 1 and 12
+function generateStar() {
+    star = Math.floor(Math.random() * 12) + 1
 }
 
+// add ball to numbers array
 function addNumber() {
     numbers.push(ball)
 }
+// add star to stars array
+function addStar() {
+    stars.push(star)
+}
 
+// check duplicated number
 function checkDuplicate() {
     if (numbers.includes(ball)) {
         console.log('numero duplicado')
@@ -68,10 +72,55 @@ function checkDuplicate() {
     }
 }
 
+// check duplicated star
+function checkDuplicateStar() {
+    if (stars.includes(star)) {
+        console.log('estrela duplicada')
+        generateStar()
+        checkDuplicateStar()
+    } else {
+        addStar()
+    }
+}
 
+// add generated numbers to htlm elements
 function writeNumbers() {
 
-    placer.insertAdjacentElement('afterbegin', div)
+    //  add title to numbers area
+    const title = `
+            <h2></h2>  
+    `
+    divNumbersEl.innerHTML = title
+    const numbersTitle = divNumbersEl.querySelector('h2')
+    divNumbersEl.insertAdjacentElement('beforebegin', numbersTitle)
+    numbersTitle.innerText = 'Números'
+
+    // create ul to place generated numbers
+    const ul_Numbers = `
+    <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    </ul>
+    `
+    divNumbersEl.innerHTML = ul_Numbers;
+
+    // add ul element to html
+    const ul_NumberEl = divNumbersEl.querySelector('ul')
+
+    const ball1 = ul_NumberEl.querySelector('ul li:nth-child(1)')
+    const ball2 = ul_NumberEl.querySelector('ul li:nth-child(2)')
+    const ball3 = ul_NumberEl.querySelector('ul li:nth-child(3)')
+    const ball4 = ul_NumberEl.querySelector('ul li:nth-child(4)')
+    const ball5 = ul_NumberEl.querySelector('ul li:nth-child(5)')
+
+    ball1.classList.add('ball')
+    ball2.classList.add('ball')
+    ball3.classList.add('ball')
+    ball4.classList.add('ball')
+    ball5.classList.add('ball')
 
     ball1.innerText = numbers[0]
     ball2.innerText = numbers[1]
@@ -80,6 +129,31 @@ function writeNumbers() {
     ball5.innerText = numbers[4]
 }
 
+function writeStars() {
+
+    // add title to stars area
+    const title = `
+            <h2></h2>  
+    `
+    divStarsEl.innerHTML = title
+    const starTitle = divStarsEl.querySelector('h2')
+    divStarsEl.insertAdjacentElement('beforebegin', starTitle)
+    starTitle.innerText = 'Estrelas'
+    
+    // create stars and add star array values
+    const packStars = `
+    <div></div>
+    <div></div>
+    `
+    divStarsEl.innerHTML = packStars
+    const star1 = divStarsEl.querySelector('div:nth-child(1)')
+    star1.classList.add('star')
+    star1.innerText = stars[0]
+    const star2 = divStarsEl.querySelector('div:nth-child(2)')
+    star2.classList.add('star')
+    star2.innerText = stars[1]
+
+}
 
 function createBalls() {
     for (let i = 0; i < 5; i++) {
@@ -92,7 +166,19 @@ function createBalls() {
         console.log(numbers[i])
     }
 
+    for (let j = 0; j < 2; j++) {
+        generateStar()
+        if (stars.length > 0) {
+            checkDuplicateStar()
+        } else {
+            addStar()
+        }
+        console.log(stars[j])
+    }
+
     writeNumbers()
+
+    writeStars()
 
     bt.classList.add('is-hidden')
     btReset.classList.add('is-visible')
